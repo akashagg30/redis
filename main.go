ae832fb7cd059fae8559546fea0406573424d35e
akashagg30 <agarwal.akash30@gmail.com>
2024-12-15 20:35:12 +0530

    removed unnecessry comments from main func

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	// log.SetOutput(os.Stdout)
 	// Start the TCP server from the server module
 	go server.StartServer(":8080", redis.MessageHandler) // Starts the server on port 8080
 
@@ -18,9 +17,4 @@ func main() {
 	log.Println("Main program running. Server is listening...")
 	redis.CleanRedisAfterDuration(time.Minute * 1)
 	select {} // Block forever, keeping the server running
-	// storage1 := storage.NewRedisStorage()
-	// storage1.Set("key1", "value1")
-	// fmt.Println(storage1.Get("key1"))
-	// storage2 := storage.NewRedisStorage()
-	// fmt.Println(storage2.Get("key1"))
 }
